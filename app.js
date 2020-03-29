930b51983573e653d12e00cde2783d8bee8487b4
mousav26 <amousavi@me.com>
2020-03-29 16:00:40 -0400

    Remove old paths no longer needed

--- app.js
+++ app.js
@@ -4,15 +4,10 @@ const path = require('path');
 const app = express();
 
 const PORT = 8080;
-const JS_DIR = 'js';
-const JS_MOUNT_PATH = '/js';
-const MODELS_DIR = 'models';
-const MODELS_MOUNT_PATH = '/models';
+const ASSET_DIR = 'public';
 const INDEX_PATH = path.join(__dirname + '/index.html');
 
-app.use(express.static('public'));
-app.use(JS_MOUNT_PATH, express.static(JS_DIR));
-app.use(MODELS_MOUNT_PATH, express.static(MODELS_DIR));
+app.use(express.static(ASSET_DIR));
 
 app.get('/', (req, res) => res.sendFile(INDEX_PATH));
 
