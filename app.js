99f819be994c62d1060561e55978750f9a5fff48
Berkmann18 <maxieberkmann@gmail.com>
2020-04-01 12:26:48 +0100

    Move index.html to /public

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const path = require('path');
 
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 const MODELS_DIR = 'models';
 const MODELS_MOUNT_PATH = '/models';
-const INDEX_PATH = path.join(`${__dirname}/index.html`);
+const INDEX_PATH = 'index.html';
 const PUBLIC_DIR = 'public';
 
 app.use(express.static(PUBLIC_DIR));
