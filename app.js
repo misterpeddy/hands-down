fa3c570b9c2e4902b05568a4e068dfc4bf4b5c6f
Berkmann18 <maxieberkmann@gmail.com>
2020-04-05 02:28:38 +0100

    Turns index/user to dev/index

--- app.js
+++ app.js
@@ -8,17 +8,15 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 const MODELS_DIR = 'models';
 const MODELS_MOUNT_PATH = '/models';
+const DEV_PATH = path.join(`${__dirname}/public/dev.html`);
 const INDEX_PATH = 'index.html';
-const USER_PATH = path.join(`${__dirname}/public/user.html`);
 const PUBLIC_DIR = 'public';
 
 app.use(express.static(PUBLIC_DIR));
 app.use(MODELS_MOUNT_PATH, express.static(MODELS_DIR));
 
-// TODO Change the '/' to '/dev' or '/collection' for index.html (to be renamed to dev.html)
+app.get('/dev', (_, res) => res.sendFile(DEV_PATH));
 app.get('/', (_, res) => res.sendFile(INDEX_PATH));
-// TODO Change the '/user' to '/' for user.html (to be renamed to index.html)
-app.get('/user', (_, res) => res.sendFile(USER_PATH));
 
 reload(app)
   .then(() => {
