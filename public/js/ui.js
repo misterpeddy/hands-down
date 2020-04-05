961acaa0242f338f6cab9c914ce4fc3fde615b91
Maximilian Berkmann <maxieberkmann@gmail.com>
2020-04-05 02:14:57 +0100

    Improves the design + adds favicons (#24)
    * Start on the 2nd design iteration
    
    It doesn't look quite like the Figma mockup yet but at least something
    
    * Updates the UI/UX design
    
    * Adds inference colour
    
    The hand and inference _score_ turns red when the _score_ is ≥ 80%
    
    * Split the project and original Skeleton styles
    
    And rectified the button colour changes
    
    * Simplifies styles
    
    * Fixes the changing button width problem
    
    * Adds favicons
    
    Resolves #29
    
    * Rectifies HTML comment
    
    * Removes useless canvas dimensions and ...
    
    Moves inline CSS code to a stylesheet for a better performance and
    separation of concerns.
    
    * Renames `flag` param to `value`
    
    * Makes the camera view more responsive
    
    That way, it gets bigger on laptop screens but not too big for
    tablets/phablets/phones.

--- public/js/ui.js
+++ public/js/ui.js
@@ -7,19 +7,27 @@ let inferenceText;
 let collectionText;
 
 // All handlers are private
-function toggleCollection() {
-  state.isCollectionOn = !state.isCollectionOn;
-  collectionButton.innerHTML = state.isCollectionOn ? 'On' : 'Off';
-}
-
-function toggleInference() {
-  state.isInferenceOn = !state.isInferenceOn;
-  inferenceButton.innerHTML = state.isInferenceOn ? 'On' : 'Off';
+function toggleButton(button, value) {
+  state[value] = !state[value];
+  /* eslint-disable no-param-reassign */
+  if (state[value]) {
+    button.innerHTML = 'On';
+    button.classList.remove('button-off');
+  } else {
+    button.innerHTML = 'Off';
+    button.classList.add('button-off');
+  }
 }
 
 function toggleLabel() {
   state.label = !state.label;
-  labelButton.innerHTML = state.label ? 'True' : 'False';
+  if (state.label) {
+    labelButton.innerHTML = 'True';
+    labelButton.classList.remove('button-off');
+  } else {
+    labelButton.innerHTML = 'False';
+    labelButton.classList.add('button-off');
+  }
 }
 
 function setStateUI(appState) {
@@ -36,8 +44,12 @@ function initButtonsUI(exportDataHandler) {
   inferenceButton.innerHTML = state.isInferenceOn ? 'On' : 'Off';
   labelButton.innerHTML = state.label ? 'True' : 'False';
 
-  collectionButton.onclick = toggleCollection;
-  inferenceButton.onclick = toggleInference;
+  collectionButton.onclick = () => {
+    toggleButton(collectionButton, 'isCollectionOn');
+  };
+  inferenceButton.onclick = () => {
+    toggleButton(inferenceButton, 'isInferenceOn');
+  };
   labelButton.onclick = toggleLabel;
   exportButton.onclick = exportDataHandler;
 
@@ -72,7 +84,10 @@ function initCanvas() {
 }
 
 function updateInferenceText(inference) {
-  inferenceText.innerHTML = inference.toFixed(4);
+  inferenceText.innerHTML = `${(inference * 100).toFixed(2)} %`;
+  const TOUCH_THRESHOLD = 0.8;
+  inferenceText.parentElement.className =
+    inference >= TOUCH_THRESHOLD ? 'danger' : '';
 }
 
 function updateCollectionText(numCollected) {
