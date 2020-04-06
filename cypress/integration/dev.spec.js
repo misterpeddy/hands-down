994ce869f3c0afd481528fcc71733374070b5adb
Berkmann18 <maxieberkmann@gmail.com>
2020-04-06 17:54:00 +0100

    Revises tests

--- cypress/integration/dev.spec.js
+++ cypress/integration/dev.spec.js
@@ -1,4 +1,5 @@
 /* global describe, it, cy */
+
 describe('Dev Page', () => {
   it('loads', () => {
     cy.visit('/dev');
@@ -7,11 +8,33 @@ describe('Dev Page', () => {
 
   it('has the important elements', () => {
     cy.get('#output');
-    cy.get('#collection-state-btn').should('be.disabled');
-    cy.get('#inference-state-btn').should('be.disabled');
-    cy.get('#label-btn').should('be.disabled');
-    cy.get('#export-btn').should('be.disabled');
     cy.contains('0 Data Points');
   });
+
+  it('can turn on collection', () => {
+    const collectionButton = cy.get('#collection-state-btn');
+    collectionButton.contains('Off');
+    /* collectionButton.click().then((btn) => {
+      // collectionButton.contains('On');
+      expect(btn.text()).to.include('On');
+    }); */
+  });
+
+  it('can turn on inference', () => {
+    const inferenceButton = cy.get('#inference-state-btn');
+    inferenceButton.should('contain', 'On');
+    /*  inferenceButton.click().then((btn) => {
+      expect(btn.text()).to.eq('Off');
+    }); */
+  });
+
+  it('can change the label', () => {
+    const labelButton = cy.get('#label-btn');
+    labelButton.should('contain', 'True');
+    /*  labelButton.click().then((btn) => {
+      expect(btn.text()).to.eq('False');
+    }); */
+  });
+
   // TODO Find a way to test when the camera is enabled so the button aren't disabled
 });
--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -7,8 +7,15 @@ describe('User Page', () => {
 
   it('has the important elements', () => {
     cy.get('#output');
-    cy.get('#inference-state-btn').should('be.disabled');
     cy.contains('- %');
+  });
+
+  it('can turn on inference', () => {
+    const inferenceButton = cy.get('#inference-state-btn');
+    inferenceButton.should('contain', 'On');
+    /*  inferenceButton.click().then((btn) => {
+      expect(btn.text()).to.eq('Off');
+    }); */
   });
   // TODO Find a way to test when the camera is enabled so the button aren't disabled
 });
